c568f563d768b0f201d48eb9873588e2cfb45d80
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-12 23:32:31 +0300

    generated component tabular

--- tables/src/app/app.module.ts
+++ tables/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabularComponent } from './tabular/tabular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabularComponent
   ],
   imports: [
     BrowserModule,
---
+++ tables/src/app/tabular/tabular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabular',
+  templateUrl: './tabular.component.html',
+  styleUrls: ['./tabular.component.css']
+})
+export class TabularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
